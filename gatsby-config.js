67372f654ae6bf8c82510a1f414ccdd286371150
Arnav <arnavpisces@gmail.com>
2024-11-28 20:10:54 +0530

    add google analytics code

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,17 @@ module.exports = {
       options: {
         endpoint: 'https://gmail.us20.list-manage.com/subscribe/post?u=23d672bbce5cd548b0c23208b&id=c5e4440d15',
       },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "G-NQJDZ9LFBR", // Replace with your actual tracking ID
+        head: true, // Puts tracking script in the head instead of the body
+        anonymize: true, // Anonymize IPs for GDPR compliance
+        respectDNT: true, // Respects "Do Not Track" settings in users' browsers
+        pageTransitionDelay: 0, // Delay in milliseconds for page transitions
+        enableWebVitalsTracking: true, // Tracks Web Vitals (Core Web Vitals metrics)
+      },
     }
   ],
 }
